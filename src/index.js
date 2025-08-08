d81e9b9e3485f26dce2f4e62642f91e21b24b04a
Josh <15219491+jkaps9@users.noreply.github.com>
2025-08-07 22:47:04 -0400

    remove unneeded js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import "./root.css";
 import "./critical.css";
 import "./local.css";
-
-const header = document.querySelector("header");
-const hamburger = document.querySelector("button.hamburger");
-
-hamburger.addEventListener("click", () => {
-  header.classList.toggle("active");
-});
